41c56c0cc9196cf90888a7880f440e842081f344
Md Forhad Sarkar <ashadullahalforhad@gmail.com>
2024-10-09 22:40:46 +0600

    update ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
+
+"use client";
 import { useForm, useFieldArray } from "react-hook-form";
 import PhoneMockup from "./components/PhoneMockup";
-import { Github, Youtube, Linkedin } from "lucide-react"; // React Lucid Icons
+import { Github, Youtube, Linkedin, ChevronDown, Link2 } from "lucide-react";
 
 export default function LinksForm() {
-  const { control, register, handleSubmit } = useForm({
+  const { control, register, handleSubmit, watch, setValue } = useForm({
     defaultValues: {
       links: [
-        { platform: "GitHub", link: "https://github.com/username" },
-        { platform: "YouTube", link: "https://youtube.com/user" },
+        { platform: "GitHub", link: "" },
+        { platform: "YouTube", link: "" },
       ],
     },
   });
@@ -20,6 +22,19 @@ export default function LinksForm() {
 
   const onSubmit = (data) => console.log(data);
 
+  const platformOptions = [
+    { value: "GitHub", label: "GitHub", icon: <Github className="mr-2" /> },
+    { value: "YouTube", label: "YouTube", icon: <Youtube className="mr-2" /> },
+    {
+      value: "LinkedIn",
+      label: "LinkedIn",
+      icon: <Linkedin className="mr-2" />,
+    },
+  ];
+
+  // Watch the platform values to get the selected platform
+  const selectedPlatforms = watch("links");
+
   return (
     <div className="container mx-auto p-8">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
@@ -30,14 +45,14 @@ export default function LinksForm() {
         <div className="p-4 bg-white rounded-lg shadow-lg border border-gray-200">
           <h2 className="text-2xl font-semibold mb-4">Customize your links</h2>
           <p>
-            add/edit/remove links below and then share all your profiles with
-            the wolrd!
+            Add/edit/remove links below and then share all your profiles with
+            the world!
           </p>
           <form onSubmit={handleSubmit(onSubmit)}>
             <button
               type="button"
               onClick={() =>
-                append({ platform: "GitHub", link: "https://github.com" })
+                append({ platform: "GitHub", link: "" })
               }
               className="block w-full mt-4 text-purple-600 border border-purple-600 rounded-lg py-2"
             >
@@ -47,13 +62,14 @@ export default function LinksForm() {
               {fields.map((item, index) => (
                 <div
                   key={item.id}
-                  className="flex items-center space-x-4 flex-col bg-slate-400 p-4"
+                  className="flex items-center space-x-4 flex-col bg-slate-200 p-4 rounded-lg"
                 >
                   <div className="w-full">
-                    <div className="flex justify-between ">
-                      <label className="block mb-1 text-sm font-medium">
-                        Platform
-                      </label>
+                    <div className="flex justify-between">
+                      <h1 className="text-lg font-bold text-gray-600">
+                        Link #{index + 1}
+                      </h1>
+
                       <button
                         type="button"
                         onClick={() => remove(index)}
@@ -63,25 +79,66 @@ export default function LinksForm() {
                       </button>
                     </div>
 
-                    <select
-                      {...register(`links.${index}.platform`)}
-                      className="w-full border-gray-300 rounded-lg p-4"
-                    >
-                      <option value="GitHub">GitHub</option>
-                      <option value="YouTube">YouTube</option>
-                      <option value="LinkedIn">LinkedIn</option>
-                    </select>
+                    <label className="block mb-1 text-sm font-medium">
+                      Platform
+                    </label>
+                    {/* Custom Select Dropdown */}
+                    <div className="relative">
+                      <div className="relative w-full border-gray-300 border rounded-lg p-4 flex items-center justify-between cursor-pointer">
+                        <div className="flex items-center space-x-2">
+                          {
+                            platformOptions.find(
+                              (option) =>
+                                option.value ===
+                                selectedPlatforms[index]?.platform
+                            )?.icon
+                          }
+                          <span>
+                            {
+                              platformOptions.find(
+                                (option) =>
+                                  option.value ===
+                                  selectedPlatforms[index]?.platform
+                              )?.label
+                            }
+                          </span>
+                        </div>
+                        <ChevronDown className="ml-2" />
+                      </div>
+
+                      {/* Select dropdown options */}
+                      <select
+                        {...register(`links.${index}.platform`)}
+                        className="absolute inset-0 opacity-0 cursor-pointer w-full h-full"
+                        onChange={(e) => {
+                          const selectedPlatform = e.target.value;
+                          setValue(`links.${index}.platform`, selectedPlatform);
+                        }}
+                      >
+                        {platformOptions.map((option) => (
+                          <option key={option.value} value={option.value}>
+                            {option.label}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
                   </div>
 
                   <div className="w-full">
                     <label className="block mb-1 text-sm font-medium">
                       Link
                     </label>
-                    <input
-                      {...register(`links.${index}.link`)}
-                      className="w-full border-gray-300 rounded-lg p-4"
-                      placeholder="https://"
-                    />
+                    <div className="flex items-center border border-gray-300 rounded-lg">
+                      {/* Add the link icon here */}
+                      <div className="flex items-center p-5">
+                        <Link2 className="w-5 h-5" />
+                      </div>
+                      <input
+                        {...register(`links.${index}.link`)}
+                        className="flex-grow p-4 rounded-lg"
+                        placeholder="https://"
+                      />
+                    </div>
                   </div>
                 </div>
               ))}
